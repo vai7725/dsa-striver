9617f16b921cfb261815e9f82b450ca9bcc547c1
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2025-08-13 10:18:02 +0530

    bubble sort done

---
+++ sorting/bubble.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble(int arr[], int n)
+{
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[] = {43, 23, 56, 45, 12};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printArr(arr, n);
+
+    cout << endl;
+
+    bubble(arr, n);
+
+    printArr(arr, n);
+
+    return 0;
+}
